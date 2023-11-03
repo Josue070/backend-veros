6d72820a238b834038aa9b3600579fdb5ba2ea62
ROLAND-fARKER <rolando.loaisiga21908138@estu.unan.edu.ni>
2023-11-03 11:15:10 -0600

    comit

--- src/catalogos/catalogos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatalogosService } from './catalogos.service';
-import { CatalogosController } from './catalogos.controller';
-
-@Module({
-  controllers: [CatalogosController],
-  providers: [CatalogosService],
-})
-export class CatalogosModule {}
